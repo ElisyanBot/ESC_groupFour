b5642ff83d3dbf1b80516ca12e3fb1ffcf1960be
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-15 21:54:50 +0100

    Add star rating function

--- src/javascript/functions/renderChallenges.js
+++ src/javascript/functions/renderChallenges.js
@@ -3,9 +3,11 @@ import { fetchData } from "./fetchData.js";
 async function renderChallenges() {
   const data = await fetchData();
   const cardSection = document.querySelector('#card-section')
+  let idOfCard = 0;
   data.forEach(data => {
     const newCard = document.createElement('article');
     newCard.setAttribute("class", "card");
+    newCard.setAttribute("id", "id" + idOfCard);
 
     newCard.innerHTML = `
       <picture class="card-top-part"><img src = ${data.image}></picture>
@@ -13,7 +15,13 @@ async function renderChallenges() {
         <div class="card-text-container">
           <h2> ${data.title} </h2>
           <div class="card-subheader-container">
-            <ul class="rating-stars"><li>${data.rating}</li></ul>
+            <ul class="rating-stars">
+            <li>&#9733;</li>
+            <li>&#9733;</li>
+            <li>&#9733;</li>
+            <li>&#9733;</li>
+            <li>&#9733;</li>
+            </ul>
             <small>${data.minParticipants} - ${data.maxParticipants} participants</small>
           </div>
           <p>${data.description}</p>
@@ -23,7 +31,28 @@ async function renderChallenges() {
         </div>
       </div>
       `;
+
     cardSection.appendChild(newCard);
+
+    function setStarsInRanking(idOfCard, rankinValue) {
+      const starsItems = document.querySelectorAll(`#id${idOfCard} .rating-stars li`);
+      let rankningNumber = rankinValue; //obs!! put ranking data from api in this param
+
+      //removes error from console
+      if (rankningNumber > 5) {
+        rankningNumber = 0
+        console.log(`ERR: challenge card ID: ${idOfCard} has has to high ranking value`)
+      }
+
+      for (let index = 0; index < rankningNumber; index++) {
+        starsItems[index].classList.add('checked')
+      }
+
+    };
+    setStarsInRanking(idOfCard, data.rating)
+
+    idOfCard++;
   });
 }
 renderChallenges();
+
