ed76cbf2e34041676e372746cd9f213b0a96bb4c
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-16 11:38:29 +0100

    Create function for rendering the three top rated challenges on index.html

---
+++ src/javascript/functions/indexRenderChallenges.js
@@ -0,0 +1,53 @@
+import { fetchData } from "./fetchData.js";
+import { setStarsInRating } from "./renderStars.js";
+
+export default async function indexRenderChallenges() {
+  const dataUnsorted = await fetchData();
+  const cardSection = document.querySelector('#card-articles-container')
+  let idOfCard = 0;
+
+  const data = dataUnsorted.sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating));
+
+  for (let index = 0; index < 3; index++) {
+    const newCard = document.createElement('article');
+    newCard.setAttribute("id", "id" + idOfCard); // Using id prefix to escape escaping when using querySelector when rendering stars
+    newCard.setAttribute("class", `card ${data[index].type}`);
+
+    data[index].labels.forEach(labels => {
+      for (let i = 0; i < labels.length; i++) {
+        newCard.classList.add(`${data[index].labels[i]}`);
+      }
+    });
+
+    newCard.innerHTML = `
+      <picture class="card-top-part"><img src = ${data[index].image}></picture>
+      <div class="card-bottom-part">
+        <div class="card-text-container">
+          <h2> ${data[index].title} </h2>
+          <div class="card-subheader-container">
+            <ul class="rating-stars">
+              <li>&#9733;</li>
+              <li>&#9733;</li>
+              <li>&#9733;</li>
+              <li>&#9733;</li>
+              <li>&#9733;</li>
+            </ul>
+            <small>
+            ${data[index].minParticipants} - ${data[index].maxParticipants} participants
+            </small>
+          </div>
+          <p>${data[index].description}</p>
+        </div>
+        <div class="card-btn-container">
+          <button class="fourth-btn">Book this room</button>
+        </div>
+      </div>
+      `;
+
+    cardSection.appendChild(newCard);
+
+    setStarsInRating(idOfCard, data[index].rating)
+
+    idOfCard++;
+  };
+}
