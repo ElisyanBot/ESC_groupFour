4c4d5d4485fe95b5268b5013da1f1139c170a115
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-16 13:28:39 +0100

    added remove tag filter functions

--- src/javascript/functions/filterByTag.js
+++ src/javascript/functions/filterByTag.js
@@ -1,30 +1,60 @@
+import {showOnlineCards, showOnsiteCards} from "./filterByType.js";
+
+let totalOfActiveTags = 0;
 export default function filterByTag(){
     console.log('hello world')
-
     //creates dynamic addEventListener on TagBtn, if we want to create more tags
     const byTagBtns = document.querySelectorAll('.filter-byTag-container button');
     byTagBtns.forEach(Element =>{
-        Element.addEventListener('click', () => displayCardWithTag(Element) )
+        Element.addEventListener('click', () => {
+            let isActive = false;
+            displayCardWithTag(Element)
+            console.log(totalOfActiveTags)
+        })
     })
 
 };
 
 //on click funtion
 function displayCardWithTag(tag){
-    console.clear();
+    if(tag.style.backgroundColor !== 'lightgray'){
+        tag.style.backgroundColor = 'lightgray'
+        findByCardTagName(tag)
+        totalOfActiveTags++;
+    }  else {
+        tag.style.backgroundColor = 'white'
+        CardTagNameRemove(tag)
+        totalOfActiveTags--;
+      
+    }
+}
 
-    tag.style.backgroundColor !== 'lightgray'? 
-        tag.style.backgroundColor = 'lightgray' : 
-        tag.style.backgroundColor = 'white';
 
-        const cards = document.querySelectorAll('.card');
+function findByCardTagName(tag){
+    const cards = document.querySelectorAll('.card');
         cards.forEach( card => {
         if(card.style.display !== 'none'){
             let newRegEx = new RegExp(`${tag.innerText}`); //kollar på classlist.value som är en sträng av alla klasser.
             if(!newRegEx.test(card.classList.value)){
                 card.style.display = 'none';
-                console.log(tag.innerText)
             }
         }
     })
+}
+
+function CardTagNameRemove(tag){
+    const cards = document.querySelectorAll('.card');
+    const cbOnline = document.querySelector("#filter-checkbox-online");
+    const cbOnsite = document.querySelector("#filter-checkbox-onsite");
+   
+    cards.forEach( card => {
+        card.style.display = 'flex';
+    })
+
+    if(cbOnline.checked == false){
+        showOnlineCards();
+    }
+    if(cbOnsite.checked == false){
+        showOnsiteCards();
+    }
 }
