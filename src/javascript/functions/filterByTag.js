b2a5e30062e524eff8989cc4861b5c8f18fb06bd
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-16 12:07:58 +0100

    feat:basic filter func, missing display multi tags

---
+++ src/javascript/functions/filterByTag.js
@@ -0,0 +1,41 @@
+export default function filterByTag(){
+    console.log('hello world')
+
+    //creates dynamic addEventListener on TagBtn, if we want to create more tags
+    const byTagBtns = document.querySelectorAll('.filter-byTag-container button');
+    byTagBtns.forEach(Element =>{
+        Element.addEventListener('click', () => displayCardWithTag(Element) )
+    })
+
+};
+
+//on click funtion
+function displayCardWithTag(tag){
+    console.clear();
+
+    tag.style.backgroundColor !== 'lightgray'? 
+        tag.style.backgroundColor = 'lightgray' : 
+        tag.style.backgroundColor = 'white';
+
+        const cards = document.querySelectorAll('.card');
+        cards.forEach( card => {
+        if(card.style.display !== 'none'){
+            let newRegEx = new RegExp(`${tag.innerText}`); //kollar på classlist.value som är en sträng av alla klasser.
+            if(!newRegEx.test(card.classList.value)){
+                card.style.display = 'none';
+                console.log(tag.innerText)
+            }
+        }
+    })
+}
+// `${tag.innerText}`
+
+ 
+            //     let testArr = card.classList
+            //     testArr.forEach( cardClass => {
+            //         if(cardClass !== `${tag.innerText}`){
+            //             card.style.backgroundColor = 'red';
+            //         } else {
+            //             card.style.backgroundColor = 'blue';
+            //         }
+            //     })
