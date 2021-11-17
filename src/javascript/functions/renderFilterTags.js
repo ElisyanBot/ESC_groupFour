a6174d9b5112690e989092b50eb8739cc53efbe0
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-17 13:12:35 +0100

    doc: added filter tag render file

---
+++ src/javascript/functions/renderFilterTags.js
@@ -0,0 +1,7 @@
+import { fetchData } from "./fetchData.js";
+// [ ] hämta labels från fetch.
+    
+// [ ] kolla igenom alla labels och spara dem i en ny array
+// [ ] filterara alla labels och om namnet är samma spara bara en av dem i ny arr
+// [ ] använd nya arrayen och skriv ut till filter sektionen alla existerande typer av taggarna.
+
