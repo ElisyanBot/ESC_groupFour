f588c10d66a335ab27387a74db0e1c113d85e00e
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-17 13:38:54 +0100

    fetch lables works + adding all lables to new arr

--- src/javascript/functions/renderFilterTags.js
+++ src/javascript/functions/renderFilterTags.js
@@ -1,7 +1,23 @@
 import { fetchData } from "./fetchData.js";
-// [ ] hämta labels från fetch.
-    
-// [ ] kolla igenom alla labels och spara dem i en ny array
+// [x] hämta labels från fetch.
+export default async function renderTagsToFilterSection(){
+    console.log('test')
+    const data = await fetchData();
+    filterToMaxOneTagPerName(getLabelsFromApi(data));
+}
+// [x] kolla igenom alla labels och spara dem i en ny array
+function getLabelsFromApi(dataApi){
+    let newLabelsAarry = []
+    for (const challenge of dataApi) {
+        newLabelsAarry.push(...challenge.labels);
+    }
+    return newLabelsAarry;
+}
 // [ ] filterara alla labels och om namnet är samma spara bara en av dem i ny arr
-// [ ] använd nya arrayen och skriv ut till filter sektionen alla existerande typer av taggarna.
+function filterToMaxOneTagPerName(array){
+    let oldArray = array
+    console.log(oldArray)
 
+    // return newArray;
+}
+// [ ] använd nya arrayen och skriv ut till filter sektionen alla existerande typer av taggarna.
