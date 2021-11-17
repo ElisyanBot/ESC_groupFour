f471aa23d2ab4d4a084769cf03fcfd79fac68721
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-17 13:14:58 +0100

    added render filter tag doc path to main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,9 +2,11 @@ import MobileMeny from './functions/mobileMeny.js';
 
 MobileMeny();
 
+//render elements
 import renderChallenges from './functions/renderChallenges.js';
-
 renderChallenges();
+import renderfilterTagsInMeny from './functions/renderFilterTags.js';
+renderfilterTagsInMeny();
 
 //filter functions
 import filterByType from './functions/filterByType.js';
