330580ac1db46a8a3bdfadf6910706bc1d846ec3
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-17 11:02:06 +0100

    Add comments

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,12 +1,15 @@
+// For all
 import MobileMeny from './functions/mobileMeny.js';
 
 MobileMeny();
 
+// For index
 import indexRenderChallenges from './functions/indexRenderChallenges.js';
 if (document.querySelector("#card-articles-container")) {
   indexRenderChallenges();
 }
 
+// For challenge
 import renderChallenges from './functions/renderChallenges.js';
 import filterByType from './functions/filterByType.js';
 if (document.querySelector("#card-section")) {
