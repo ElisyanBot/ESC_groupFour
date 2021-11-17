1c64e0213f1d4c1118ed767f44011b785a06d159
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-17 14:17:52 +0100

    func: added rendered tags + working tag btn func

--- src/javascript/functions/renderFilterTags.js
+++ src/javascript/functions/renderFilterTags.js
@@ -1,9 +1,14 @@
 import { fetchData } from "./fetchData.js";
+import filterByTag from './filterByTag.js';
 // [x] hämta labels från fetch.
 export default async function renderTagsToFilterSection(){
     console.log('test')
     const data = await fetchData();
     let sorterdTags = filterToMaxOneTagPerName(getLabelsFromApi(data));
+    renderToFilterByTag(sorterdTags);
+    
+    //add btn finctionallity to all tags
+    filterByTag(); 
 }
 // [x] kolla igenom alla labels och spara dem i en ny array
 function getLabelsFromApi(dataApi){
@@ -13,9 +18,18 @@ function getLabelsFromApi(dataApi){
     }
     return newLabelsAarry;
 }
-// [ ] filterara alla labels och om namnet är samma spara bara en av dem i ny arr
+// [x] filterara alla labels och om namnet är samma spara bara en av dem i ny arr
 function filterToMaxOneTagPerName(array){
     let newArray = array.filter((item, index)=> array.indexOf(item) === index)
     return newArray;
 }
 // [ ] använd nya arrayen och skriv ut till filter sektionen alla existerande typer av taggarna.
+function renderToFilterByTag(arrayOfTags){
+    const byTagSecttion = document.querySelector('.filter-byTag-container');
+    let output = ''
+    arrayOfTags.forEach(element => {
+        output += `<button>${element}</button>`
+    });
+    byTagSecttion.innerHTML = output
+
+}
