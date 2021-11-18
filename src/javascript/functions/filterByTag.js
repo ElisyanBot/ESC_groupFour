ed62af1013a2366d579545cf71f910472c4fcaa3
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-18 15:27:18 +0100

    bug fix: typing error in kode

--- src/javascript/functions/filterByTag.js
+++ src/javascript/functions/filterByTag.js
@@ -29,7 +29,7 @@ function getObjIndex(tagBtnText){
 //creates dynamic addEventListener on TagBtn, if the labels(tags) change
 export default function filterByTag(){
     const byTagBtns = document.querySelectorAll('.filter-byTag-container button');
-    
+
     byTagBtns.forEach(tagBtn =>{
         tagStorageArray.push(createTagObj(tagBtn.innerText));
         tagBtn.addEventListener('click', () => {
@@ -55,44 +55,46 @@ function CardTagNameRemove(){
     const cards = document.querySelectorAll('.card');
     const cbOnline = document.querySelector("#filter-checkbox-online");
     const cbOnsite = document.querySelector("#filter-checkbox-onsite");
-   
+    
     cards.forEach( card => {
         card.style.display = 'flex';
         card.style.backgroundColor = 'white';
     })
-
+    
     if(cbOnline.checked == false) showOnlineCards();
     if(cbOnsite.checked == false) showOnsiteCards();
 }
 
 //displays card if the tag(label) is ative
 function displayCardsForActiveTags(){
-    const aktiveTags = pushActiveTagsToArray();
-    aktiveTags.forEach(index =>{
+    const activeTags = pushActiveTagsToArray();
+    activeTags.forEach(index =>{
         findByCardTagName(index)
+        console.log('works')
     })
 } 
 
 
 //looking at the tagStorage for of tabObj.active == true
-function checkTotalAtkiveTags(){
-    let totalAktiveTags = 0
+function checkTotalActiveTags(){
+    let totalActiveTags = 0
     for (const tagObj of tagStorageArray){
-        if(tagObj.aktive == true) totalAktiveTags++;
+        if(tagObj.active == true) totalActiveTags++;
     }
-    return totalAktiveTags;
+    return totalActiveTags;
 }
 
-//stores all aktive tags in an new array
+//stores all active tags in an new array
 function pushActiveTagsToArray(){
-    let totalAktiveTags = checkTotalAtkiveTags();
-    const aktivTagsArray = [];
-    if(totalAktiveTags > 0){
+    let totalActiveTags = checkTotalActiveTags();
+    const activTagsArray = [];
+    if(totalActiveTags > 0){
         for (const tagObj of tagStorageArray){
-            if(tagObj.aktive == true) aktivTagsArray.push(tagObj.tagName);
+            if(tagObj.active == true) activTagsArray.push(tagObj.tagName);
         }
     }
-    return aktivTagsArray;
+    console.log(activTagsArray)
+    return activTagsArray;
 }
 
 
@@ -100,6 +102,7 @@ function pushActiveTagsToArray(){
 function findByCardTagName(tagIndex){
     const cards = document.querySelectorAll('.card');
     cards.forEach( card => {
+        console.log('works')
         if(card.style.display === 'none') return //om ett kort har display: none; returnera inget annars fortsätt
         let newRegEx = new RegExp(`${tagIndex}`); //kollar på classlist.value som är en sträng av alla klasser.
         if(!newRegEx.test(card.classList.value)){
