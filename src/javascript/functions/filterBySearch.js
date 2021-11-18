5f7788ed8bd93b26835231a8a6929464b35f56f2
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-18 17:34:04 +0100

    Changed name of variable p to desc

--- src/javascript/functions/filterBySearch.js
+++ src/javascript/functions/filterBySearch.js
@@ -5,20 +5,20 @@ export default async function filterBySearch() {
 
     const cards = document.querySelectorAll('article');
     const input = document.querySelector('#filter-search-input');
-    input.addEventListener('keyup', ()=> {
-        
+    input.addEventListener('keyup', () => {
+
         const inputValue = input.value.toUpperCase();
-        for(let i = 0; i < cards.length; i++) {
+        for (let i = 0; i < cards.length; i++) {
             let title = cards[i].querySelector('article h2');
-            let p = cards[i].querySelector('article p');
-        
-        if(title.textContent.toUpperCase().indexOf(inputValue) > -1 || p.textContent.toLocaleUpperCase().indexOf(inputValue) > -1) {
+            let desc = cards[i].querySelector('article p');
+
+            if (title.textContent.toUpperCase().indexOf(inputValue) > -1 || desc.textContent.toLocaleUpperCase().indexOf(inputValue) > -1) {
                 cards[i].style.display = "flex";
             } else {
                 cards[i].style.display = "none";
             }
         }
-    })     
+    })
 }
 
 
