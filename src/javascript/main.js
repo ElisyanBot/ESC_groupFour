abc533f560efa3e843851f3bd9b203495e84c6fd
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2021-11-19 17:27:10 +0100

    Swap out all filter functions to enableFilters.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -9,19 +9,11 @@ if (document.querySelector("#card-articles-container")) {
 }
 
 // For challenge
-import showFilters from './functions/showFilters.js';
+import enableFilters from './functions/enableFilters.js';
 import renderChallenges from './functions/renderChallenges.js';
-import filterByType from './functions/filterByType.js';
-import filterBySearch from './functions/filterBySearch.js';
-import { byRatingFilterBtns } from './functions/filterByRating.js';
 
-import renderfilterTagsInMeny from './functions/renderFilterTags.js';
 
 if (document.querySelector("#card-section")) {
   renderChallenges();
-  filterBySearch();
-  filterByType();
-  byRatingFilterBtns();
-  renderfilterTagsInMeny();
-  showFilters()
+  enableFilters();
 };
