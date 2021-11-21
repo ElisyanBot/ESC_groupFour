cba1f9cf3e34a4cf69f34ad0f0296fb0fc186e22
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-21 09:43:45 +0100

    func: working card filter by rating

--- src/javascript/functions/filterByRating.js
+++ src/javascript/functions/filterByRating.js
@@ -1,50 +1,74 @@
-// [] när man klickar på en stjäna ska man få ett värede från 1 - 5
+// [x] när man klickar på en stjäna ska man få ett värede från 1 - 5
 // [x] lyckas få stjärnorna att fyllas i till och med värdet som är valt
-// [] skapa ett objekt som kommer ihåg värdet som är valt
+// [x] skapa ett objekt som kommer ihåg värdet som är valt
 
+export const storage = { 
+    lessThanValue: 0, 
+    biggerThanValue: 5
+};
 
 export function byRatingFilterBtns(){
     const biggerThanList = document.querySelector('#rating-biggerThan');
-    const biggerThanItem = document.querySelectorAll('#rating-biggerThan > li');
     const lessThanList = document.querySelector('#rating-lessThan');
+    const biggerThanItem = document.querySelectorAll('#rating-biggerThan > li');
     const lessThanItem = document.querySelectorAll('#rating-lessThan > li');
     
-    let lessThanNumber = setRatingFilter(lessThanList, lessThanItem, 0);
-    let biggerThanNumber = setRatingFilter(biggerThanList, biggerThanItem, 5);
-
-    console.log(biggerThanNumber,  lessThanNumber)
-}
-
-function setRatingFilter(starRatingListId, starRatingItemArr, startingValue){
-    let number = startingValue;
-    
-    starRatingListId.addEventListener('click', (e) =>{
-        let arr = Array.prototype.slice.call(starRatingItemArr);
-
-        //clears all stars
-        for (let index = 0; index < arr.length; index++) {
-            starRatingItemArr[index].classList.remove('checked')    
-          }
-
-        //geting the set value
-        //funkar det att skapa en function av detta?
-        if(e.target !== starRatingListId){
-            arr.forEach(Element =>{
-                if(e.target === Element){
-                    number = arr.indexOf(Element) + 1;
-                }  
-            })   
-        }
-
-        //sets new number of stars
-        for (let index = 0; index < number; index++) {
-            starRatingItemArr[index].classList.add('checked')    
-          }
-        }
-    )
  
-    return number
+    
+    lessThanList.addEventListener('click', (e) => {
+        storage.lessThanValue = setRatingFilter(e, lessThanItem, lessThanItem);
+        displayCards(storage.lessThanValue, storage.biggerThanValue);
+    });
+    biggerThanList.addEventListener('click', (e) => {
+        storage.biggerThanValue = setRatingFilter(e, biggerThanItem, biggerThanItem);
+        displayCards(storage.lessThanValue, storage.biggerThanValue);
+     });
 }
 
-//[] vad händer om man clickar i att minsta vädet ska vara större än högsta?
+//filter-meny stars functionality
+function setRatingFilter(e, starRatingItemArr, starRatingListId){  
+        let arr = Array.prototype.slice.call(starRatingItemArr);
+        let ratingNumber = 0;
+        //clears all stars
+        for (let index = 0; index < arr.length; index++)
+            starRatingItemArr[index].classList.remove('checked');    
+        //geting the set value
+        if(e.target !== starRatingListId){
+            arr.forEach(liStar =>{
+                if(e.target !== liStar) return;
+                ratingNumber = arr.indexOf(liStar) + 1; 
+            });   
+        }
+        //sets new number of stars
+        for (let index = 0; index < ratingNumber; index++)
+             starRatingItemArr[index].classList.add('checked');
+        return ratingNumber
+}
+
+//counts every checkd ratingStar on card.
+function getRatingFromCard(card){
+    let cardRating = 0;
+    card.querySelectorAll('ul > li').forEach(star => {
+        if(card.style.display !== "none")
+        if(star.classList.value == 'checked') cardRating++;
+    });
+    return cardRating;
+}
+// [x] funktion som gömmer kortet om stjärnona är mer eller mindre
+// - om ena är större än det andra, dölj de som inte stämmer
+// -
+//[x] vad händer om man clickar i att minsta vädet ska vara större än högsta?
 //  -- om lessthan is greater than biggerThan byt ordningen?
+
+    function displayCards(lessThan, biggerThan){
+        document.querySelectorAll('.card').forEach( card => {
+        let checkOrderOfInput = lessThan < biggerThan ? 
+        getRatingFromCard(card) < lessThan || getRatingFromCard(card) > biggerThan :
+        getRatingFromCard(card) > lessThan || getRatingFromCard(card) < biggerThan ;
+        
+            if(checkOrderOfInput){
+                if(card.style.display == "none") return;
+                card.style.display = "none";
+            };
+        })
+    }
