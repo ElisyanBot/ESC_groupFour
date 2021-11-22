88e447890455c2e12c067f70fd03c2ebb72ab884
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2021-11-22 21:00:02 +0100

    fix so fetchData can be reached everywhere

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,20 +1,27 @@
-const body = document.querySelector('body');
-// For all
-import MobileMeny from './functions/mobileMeny.js';
-MobileMeny();
-
 import { fetchData } from "./functions/fetchData.js";
 const data = await fetchData();
 
+const body = document.querySelector('body');
+
+
+// For all
+import MobileMeny from './functions/mobileMeny.js';
+import bookChallenge from './functions/bookChallenge.js';
+MobileMeny();
+bookChallenge(data);
+
 // For index
 import indexRenderChallenges from './functions/indexRenderChallenges.js';
+
 if (body.className == "index") {
   indexRenderChallenges(data);
+  
 }
 
 // For challenge
 import renderChallenges from './functions/renderChallenges.js';
 import enableFilters from './functions/enableFilters.js';
+
 if (body.className == "challengesPage") {
   enableFilters(data);
   renderChallenges(data);
