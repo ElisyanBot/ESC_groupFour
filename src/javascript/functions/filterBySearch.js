cfe0f52890ff5857badf2dac0d66d3368e7803c1
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2021-11-22 15:45:59 +0100

    doc: code splited up for easier use/export

--- src/javascript/functions/filterBySearch.js
+++ src/javascript/functions/filterBySearch.js
@@ -1,20 +1,26 @@
-export default function filterBySearch() {
-    const cards = document.querySelectorAll('article');
-    const input = document.querySelector('#filter-search-input');
-    input.addEventListener('keyup', () => {
+import activeFilters from "./activeFilterCheck.js";
 
-        const inputValue = input.value.toUpperCase();
-        for (let i = 0; i < cards.length; i++) {
-            let title = cards[i].querySelector('article h2');
-            let desc = cards[i].querySelector('article p');
-
-            if (title.textContent.toUpperCase().indexOf(inputValue) > -1 || desc.textContent.toLocaleUpperCase().indexOf(inputValue) > -1) {
-                cards[i].style.display = "flex";
-            } else {
-                cards[i].style.display = "none";
-            }
-        }
+export default function filterBySearchInput(){
+    document.querySelector('#filter-search-input').addEventListener('keyup', () => {
+        activeFilters();
+        LookForSearchInputValue();
     })
 }
 
+export function LookForSearchInputValue(){
+    const cards = document.querySelectorAll('article');
+    const inputValue = document.querySelector('#filter-search-input').value.toUpperCase();
+    
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector('article h2');
+        let desc = cards[i].querySelector('article p');
 
+        if(title.textContent.toUpperCase().indexOf(inputValue) > -1 ||
+           desc.textContent.toLocaleUpperCase().indexOf(inputValue) > -1) {
+            if(cards[i].style.display === "none") return;
+            cards[i].style.display = "flex";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
