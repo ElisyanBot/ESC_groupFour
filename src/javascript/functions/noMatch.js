e5847595b5e65fd198996f06ede2bd5b3371fdd9
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-23 13:30:14 +0100

    Add classes to div and p

--- src/javascript/functions/noMatch.js
+++ src/javascript/functions/noMatch.js
@@ -1,12 +1,12 @@
 export default function noMatches() {
-  const noMatchesDiv = document.querySelector('.no-matches');
+  const noMatchesDiv = document.querySelector('.no-matches-container');
   const cards = document.querySelectorAll('article');
   let cardsArray = Array.prototype.slice.call(cards);
 
   const displayNone = (card) => card.style.display == 'none';
 
   if (cardsArray.every(displayNone)) {
-    noMatchesDiv.innerHTML = '<h1>No matching challenges</h1>'
+    noMatchesDiv.innerHTML = '<p class="no-matches-text">No matching challenges</p>'
   }
   else {
     noMatchesDiv.innerHTML = ''
