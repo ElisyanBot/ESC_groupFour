c90f5b2cfdeb3b2518b9d46fc8ae1e57b825d5c4
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-23 13:28:20 +0100

    Add no matches function

--- src/javascript/functions/Filter/filterByTag.js
+++ src/javascript/functions/Filter/filterByTag.js
@@ -1,46 +1,47 @@
-import {showOnlineCards, showOnsiteCards} from "./filterByType.js";
+import { showOnlineCards, showOnsiteCards } from "./filterByType.js";
 import activeFilters from "./activeFilterCheck.js";
+import noMatches from "../noMatch.js";
 
 const tagStorageArray = []
 //Factory function, creates an tag(label) obj
-function createTagObj(tagName){
-    return{
+function createTagObj(tagName) {
+    return {
         tagName: tagName,
         active: false,
-        toggleActiveStatus: function(){
-            this.active === false ? 
-            this.active = true : 
-            this.active = false;
+        toggleActiveStatus: function () {
+            this.active === false ?
+                this.active = true :
+                this.active = false;
         }
     }
 }
 
 //fetch the index of an tagObj if the tabBtn has the same name.
-function getObjIndex(tagBtnText){
+function getObjIndex(tagBtnText) {
     let indexOfTag = 0;
     for (const tag of tagStorageArray) {
-        if (tagBtnText === tag.tagName) 
-        indexOfTag = tagStorageArray.indexOf(tag);
+        if (tagBtnText === tag.tagName)
+            indexOfTag = tagStorageArray.indexOf(tag);
     }
     return indexOfTag;
 }
 
 /* ========================= main function ==================================*/
 //creates dynamic addEventListener on TagBtn, if the labels(tags) change
-export default function filterByTag(){
+export default function filterByTag() {
     document.querySelectorAll('.filter-byTag-container button')
-    .forEach(tagBtn => {
-        tagStorageArray.push(createTagObj(tagBtn.innerText));
-        tagBtn.addEventListener('click', () => {
-            isTagAktive(tagBtn, getObjIndex(tagBtn.innerText));
-            activeFilters(); //bytas ut emot aktiveTagsCheck.js
+        .forEach(tagBtn => {
+            tagStorageArray.push(createTagObj(tagBtn.innerText));
+            tagBtn.addEventListener('click', () => {
+                isTagAktive(tagBtn, getObjIndex(tagBtn.innerText));
+                activeFilters(); //bytas ut emot aktiveTagsCheck.js
+            })
         })
-    })
 };
 
 //behövs denna funktionen?
-function isTagAktive(tagBtn, tagObjIndex){
-    if(tagBtn.style.backgroundColor !== 'lightgray') 
+function isTagAktive(tagBtn, tagObjIndex) {
+    if (tagBtn.style.backgroundColor !== 'lightgray')
         tagBtn.style.backgroundColor = 'lightgray';
     else {
         tagBtn.style.backgroundColor = 'white';
@@ -49,42 +50,43 @@ function isTagAktive(tagBtn, tagObjIndex){
 }
 
 //displays card if the tag(label) is ative
-export function displayCardsForActiveTags(){
+export function displayCardsForActiveTags() {
     const activeTagsArray = pushActiveTagsToArray();
-    activeTagsArray.forEach(index =>{
+    activeTagsArray.forEach(index => {
         findByCardTagName(index)
     })
-} 
+    noMatches();
+}
 
 //looking at the tagStorage for of tabObj.active == true
-function checkTotalActiveTags(){
+function checkTotalActiveTags() {
     let totalActiveTags = 0
-    for (const tagObj of tagStorageArray){
-        if(tagObj.active == true) totalActiveTags++;
+    for (const tagObj of tagStorageArray) {
+        if (tagObj.active == true) totalActiveTags++;
     }
     return totalActiveTags;
 }
 
 //stores all active tags in an new array
-function pushActiveTagsToArray(){
+function pushActiveTagsToArray() {
     let totalActiveTags = checkTotalActiveTags();
     const activTagsArray = [];
-    if(totalActiveTags > 0){
-        for (const tagObj of tagStorageArray){
-            if(tagObj.active == true) activTagsArray.push(tagObj.tagName);
+    if (totalActiveTags > 0) {
+        for (const tagObj of tagStorageArray) {
+            if (tagObj.active == true) activTagsArray.push(tagObj.tagName);
         }
     }
     return activTagsArray;
 }
 
 //finds card with matching challenges classList.value 
-function findByCardTagName(tagIndex){
+function findByCardTagName(tagIndex) {
     const cards = document.querySelectorAll('.card');
-    cards.forEach( card => {
-        if(card.style.display === 'none') return;
+    cards.forEach(card => {
+        if (card.style.display === 'none') return;
         //searsch thorugh card.classlist.value after string match
         let newRegEx = new RegExp(`${tagIndex}`);
-        if(!newRegEx.test(card.classList.value)){
+        if (!newRegEx.test(card.classList.value)) {
             card.style.display = 'none';
         }
     })
