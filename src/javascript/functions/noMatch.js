a70ec60684aa603756fbedc1ca4777f51c3b8a39
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-23 12:30:28 +0100

    Add function for displaying message when no matches are found

---
+++ src/javascript/functions/noMatch.js
@@ -0,0 +1,16 @@
+export default function noMatches() {
+  const noMatchesDiv = document.querySelector('.no-matches');
+  const cards = document.querySelectorAll('article');
+  let cardsArray = Array.prototype.slice.call(cards);
+  console.log(cardsArray);
+
+  const displayNone = (card) => card.style.display == 'none';
+
+  if (cardsArray.every(displayNone)) {
+    noMatchesDiv.innerHTML = '<h1>No matching challenges</h1>'
+  }
+  else {
+    noMatchesDiv.innerHTML = ''
+  }
+}
+
